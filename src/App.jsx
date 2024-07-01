d410e652eb8f570352a640c739c3c953125e2195
Angel Heredia <71797284+angelheredia138@users.noreply.github.com>
2024-06-30 18:52:52 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import React, { useEffect, useState } from "react";
+import { Box, Select, Center, VStack, HStack } from "@chakra-ui/react";
 import PlayerControls from "./components/PlayerControls";
 import VolumeControl from "./components/VolumeControl";
 import RotatingLogo from "./components/RotatingLogo";
 import SpotifyAuth from "./components/SpotifyAuth";
 import CurrentlyPlaying from "./components/CurrentlyPlaying";
 import Logout from "./components/Logout";
-import { Center, VStack, HStack, Box } from "@chakra-ui/react";
 import "./styles/GradientBackground.css";
+
+const gradients = [
+  { name: "Ocean Blue", value: "linear-gradient(45deg, #00c6ff, #0072ff)" },
+  { name: "Purple Dream", value: "linear-gradient(45deg, #a18cd1, #fbc2eb)" },
+  { name: "Mint", value: "linear-gradient(45deg, #00c9ff, #92fe9d)" },
+  { name: "Peach", value: "linear-gradient(45deg, #ff9a8b, #ff6a88)" },
+  { name: "Neon", value: "linear-gradient(45deg, #cc2b5e, #753a88)" },
+  { name: "Aqua", value: "linear-gradient(45deg, #13547a, #80d0c7)" },
+  { name: "Sunrise", value: "linear-gradient(45deg, #ff512f, #f09819)" },
+  { name: "Monochrome", value: "linear-gradient(45deg, #667db6, #0082c8)" },
+  { name: "Modern", value: "linear-gradient(45deg, #ff9a9e, #fad0c4)" },
+  { name: "Blush", value: "linear-gradient(45deg, #ff9a8b, #ff6a88)" },
+  { name: "Pastel", value: "linear-gradient(45deg, #d9a7c7, #fffcdc)" },
+  { name: "Calm", value: "linear-gradient(45deg, #c1dfc4, #deecdd)" },
+  { name: "Sunset", value: "linear-gradient(45deg, #f9d423, #ff4e50)" },
+  { name: "Fresh", value: "linear-gradient(45deg, #b3ffab, #12fff7)" },
+  { name: "Sweet", value: "linear-gradient(45deg, #ff9a9e, #ffdde1)" },
+  { name: "Vivid", value: "linear-gradient(45deg, #fc354c, #0abfbc)" },
+  { name: "Cool", value: "linear-gradient(45deg, #fc466b, #3f5efb)" },
+  { name: "Rainbow", value: "linear-gradient(45deg, #00d2ff, #3a7bd5)" },
+];
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isPortrait, setIsPortrait] = useState(
     window.innerHeight > window.innerWidth
   );
+  const [background, setBackground] = useState("");
 
   const handleOrientationChange = () => {
     setIsPortrait(window.innerHeight > window.innerWidth);
+  };
+
+  const handleBackgroundChange = (event) => {
+    setBackground(event.target.value);
   };
 
   useEffect(() => {
@@ -24,37 +50,10 @@ function App() {
       setIsAuthenticated(true);
     }
 
-    // Define the gradient backgrounds
-    const gradients = [
-      "linear-gradient(45deg, #00c6ff, #0072ff, #00c6ff, #0072ff, #00c6ff, #0072ff)", // Ocean Blue
-      "linear-gradient(45deg, #a18cd1, #fbc2eb, #a18cd1, #fbc2eb, #a18cd1, #fbc2eb)", // Purple Dream
-      "linear-gradient(45deg, #00c9ff, #92fe9d, #00c9ff, #92fe9d, #00c9ff, #92fe9d)", // Mint
-      "linear-gradient(45deg, #ff9a8b, #ff6a88, #ff99ac, #ff6a88, #ff9a8b, #ff99ac)", // Peach
-      "linear-gradient(45deg, #cc2b5e, #753a88, #cc2b5e, #753a88, #cc2b5e, #753a88)", // Neon
-      "linear-gradient(45deg, #13547a, #80d0c7, #13547a, #80d0c7, #13547a, #80d0c7)", // Aqua
-      "linear-gradient(45deg, #ff512f, #f09819, #ff512f, #f09819, #ff512f, #f09819)", // Sunrise
-      "linear-gradient(45deg, #667db6, #0082c8, #0082c8, #667db6, #667db6, #667db6)", // Monochrome
-      "linear-gradient(45deg, #ff9a9e, #fad0c4, #fad0c4, #ff9a9e, #ff9a9e, #ff9a9e)", // Modern
-      "linear-gradient(45deg, #ff9a8b, #ff6a88, #ff6a88, #ff9a8b, #ff9a8b, #ff9a8b)", // Blush
-      "linear-gradient(45deg, #d9a7c7, #fffcdc, #d9a7c7, #fffcdc, #d9a7c7, #fffcdc)", // Pastel
-      "linear-gradient(45deg, #c1dfc4, #deecdd, #c1dfc4, #deecdd, #c1dfc4, #deecdd)", // Calm
-      "linear-gradient(45deg, #f9d423, #ff4e50, #f9d423, #ff4e50, #f9d423, #ff4e50)", // Sunset
-      "linear-gradient(45deg, #b3ffab, #12fff7, #b3ffab, #12fff7, #b3ffab, #12fff7)", // Fresh
-      "linear-gradient(45deg, #ff9a9e, #ffdde1, #ff9a9e, #ffdde1, #ff9a9e, #ffdde1)", // Sweet
-      "linear-gradient(45deg, #fc354c, #0abfbc, #fc354c, #0abfbc, #fc354c, #0abfbc)", // Vivid
-      "linear-gradient(45deg, #fc466b, #3f5efb, #fc466b, #3f5efb, #fc466b, #3f5efb)", // Cool
-      "linear-gradient(45deg, #00d2ff, #3a7bd5, #00d2ff, #3a7bd5, #00d2ff, #3a7bd5)", // Rainbow
-    ];
-
     // Select a random gradient
     const randomGradient =
-      gradients[Math.floor(Math.random() * gradients.length)];
-
-    // Apply the random gradient to the background
-    document.body.style.background = randomGradient;
-    document.body.style.backgroundSize = "600% 600%";
-    document.body.style.animation =
-      "GradientBackgroundAnimation 16s ease infinite";
+      gradients[Math.floor(Math.random() * gradients.length)].value;
+    setBackground(randomGradient);
 
     // Add event listener for orientation change
     window.addEventListener("resize", handleOrientationChange);
@@ -64,12 +63,37 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    document.body.style.background = background;
+    document.body.style.backgroundSize = "600% 600%";
+    document.body.style.animation =
+      "GradientBackgroundAnimation 16s ease infinite";
+  }, [background]);
+
   return (
     <Center h="100vh">
       {isAuthenticated ? (
         isPortrait ? (
           <VStack spacing={8}>
             <RotatingLogo />
+            <Select
+              placeholder="Select Background"
+              onChange={handleBackgroundChange}
+              value={background}
+              mt={4}
+              color="white"
+              bg="rgba(0, 0, 0, 0.5)"
+              borderRadius="md"
+              borderColor="white"
+              _hover={{ borderColor: "gray.500" }}
+              _focus={{ borderColor: "gray.500" }}
+            >
+              {gradients.map((gradient) => (
+                <option key={gradient.name} value={gradient.value}>
+                  {gradient.name}
+                </option>
+              ))}
+            </Select>
             <PlayerControls />
             <VolumeControl />
             <CurrentlyPlaying />
@@ -88,6 +112,28 @@ function App() {
               </VStack>
             </HStack>
             <CurrentlyPlaying />
+            <Select
+              placeholder="Select Background"
+              focusBorderColor="teal.500"
+              borderColor={"#319795"}
+              background={"#319795"}
+              textColor={"black"}
+              fontSize="1em"
+              fontWeight="semibold"
+              fontFamily="inherit"
+              borderRadius="8px"
+              onChange={handleBackgroundChange}
+              value={background}
+              mt={4}
+              _hover={{ borderColor: "gray.500" }}
+              _focus={{ borderColor: "gray.500" }}
+            >
+              {gradients.map((gradient) => (
+                <option key={gradient.name} value={gradient.value}>
+                  {gradient.name}
+                </option>
+              ))}
+            </Select>
           </VStack>
         )
       ) : (
